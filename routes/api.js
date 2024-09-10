adc69b15f1684dd457a291c4abaf6b72885cb7ab
Josh S <spajos07@gmail.com>
2024-09-10 22:53:11 +0200

    Refined Api Added landing page

--- routes/api.js
+++ routes/api.js
@@ -3,8 +3,18 @@ const db = require("../db");
 var router = express.Router();
 
 /* GET home page. */
-router.get("/", async function (req, res, next) {
-    res.send(await db.get()); // TODO
+router.get("/:date?", async function (req, res, next) {
+    let data = await db.get(req.params.date);
+    if (!data) {
+        res.status(404);
+        return res.send(
+            req.params.date
+                ? `No entry on ${req.params.date}`
+                : "No date specified. Please provide a date in url format /api/DD.MM.YYYY",
+        );
+    }
+    res.status(200);
+    res.send(data);
 });
 
 module.exports = router;
