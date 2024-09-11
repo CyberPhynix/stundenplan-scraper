05402660f4b62cc2e52eb3a08426106d1109d71e
Josh S <spajos07@gmail.com>
2024-09-11 21:52:59 +0200

    Added server compatibility

--- scraper.js
+++ scraper.js
@@ -17,7 +17,9 @@ const setupScraper = async () => {
 const scrapeSchedule = async () => {
     let browser;
     try {
-        browser = await puppeteer.launch();
+        browser = await puppeteer.launch({
+            executablePath: "/opt/render/.cache/puppeteer/chrome/linux-128.0.6613.119/chrome-linux64/chrome",
+        });
         const page = await browser.newPage();
 
         await login(page);
